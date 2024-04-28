8f144673f8c0160e2f2451577131d645f1bd71ac
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-28 17:15:16 +0530

    Format changes.

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let progress = document.getElementById('progress');
 let song = document.getElementById('song');
 let ctrlIcon = document.getElementById('ctrlIcon');
 
-song.onloadedmetadata = function() {
+song.onloadedmetadata = function () {
     progress.max = song.duration;
     progress.value = song.currentTime;
 
 }
 
 function playPause() {
-    if(ctrlIcon.classList.contains('fa-pause')) {
+    if (ctrlIcon.classList.contains('fa-pause')) {
         song.pause();
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play")
     }
-    else{
+    else {
         song.play();
         ctrlIcon.classList.add("fa-pause");
         ctrlIcon.classList.remove("fa-play")
@@ -22,13 +22,13 @@ function playPause() {
 }
 
 
-if(song.play()) {
+if (song.play()) {
     setInterval(() => {
         progress.value = song.currentTime;
     }, 500);
 }
 
-progress.onchange = function() {
+progress.onchange = function () {
     song.play();
     song.currentTime = progress.value;
     ctrlIcon.classList.add("fa-pause");
